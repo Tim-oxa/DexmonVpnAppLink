345673484aa7b339e0be343bf24e43b525296525
Tim <113267366+Tim-oxa@users.noreply.github.com>
2025-05-22 03:19:14 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-PATH = "/app"
-MAC_URL = "https://apps.apple.com/us/app/happ-proxy-utility/id6504287215"
-WINDOWS_URL = "https://github.com/Happ-proxy/happ-desktop/releases/latest/download/setup-Happ.x86.exe"
-IOS_URL = "https://apps.apple.com/us/app/happ-proxy-utility/id6504287215"
-ANDROID_URL = "https://play.google.com/store/apps/details?id=com.happproxy"
-OTHER_URL = "https://happ.su"
-PORT = 116
